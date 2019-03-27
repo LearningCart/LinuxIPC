2dc1f444697a726addb9cc6f2c15da432a355d0e
Jatin Gandhi <jatinkgandhi@gmail.com>
2019-03-27 10:42:18 +0530

    Set affinity of a thread to the core
    Set thread affinity to specific core.

---
+++ attachtocore.c
@@ -0,0 +1,14 @@
+int stick_this_thread_to_core(int core_id) {
+   int num_cores = sysconf(_SC_NPROCESSORS_ONLN);
+   if (core_id < 0 || core_id >= num_cores)
+      return EINVAL;
+
+   cpu_set_t cpuset;
+   CPU_ZERO(&cpuset);
+   CPU_SET(core_id, &cpuset);
+
+   pthread_t current_thread = pthread_self();    
+   return pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpuset);
+}
+
+/* ps -mo pid,tid,%cpu,psr -p `pgrep BINARY-NAME` */
